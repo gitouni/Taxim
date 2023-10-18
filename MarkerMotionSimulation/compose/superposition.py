b00c1561370095372800935a2b901aab58f76830
gitouni <30369762+gitouni@users.noreply.github.com>
2023-10-18 08:46:53 +0100

    Add files via upload

--- MarkerMotionSimulation/compose/superposition.py
+++ MarkerMotionSimulation/compose/superposition.py
@@ -4,24 +4,42 @@ import os
 from os import path as osp
 import cv2
 from scipy.optimize import nnls
-from scipy.ndimage import correlate
-import scipy.ndimage as ndimage
+# from scipy.ndimage import correlate
+# import scipy.ndimage as ndimage
 from scipy import interpolate
-
+os.chdir(osp.dirname(__file__))
 import sys
 sys.path.append("..")
-from compose.dataLoader import dataLoader
 sys.path.append("../..")
+from compose.dataLoader import dataLoader
 import Basics.sensorParams as psp
 import Basics.params as pr
 
 def cropMap(deformMap):
     # from d*d to h*w
-    channel = deformMap.shape[0]
-    croppedMap = np.zeros((channel,psp.h,psp.w))
-    for c in range(channel):
-        croppedMap[c,:,:] = deformMap[c,psp.d//2-psp.h//2:psp.d//2+psp.h//2,psp.d//2-psp.w//2:psp.d//2+psp.w//2]
+    if np.ndim(deformMap) == 3:
+        channel = deformMap.shape[0]
+        croppedMap = np.zeros((channel,psp.h,psp.w))
+        for c in range(channel):
+            croppedMap[c,:,:] = deformMap[c,psp.d//2-psp.h//2:psp.d//2+psp.h//2,psp.d//2-psp.w//2:psp.d//2+psp.w//2]
+    elif np.ndim(deformMap) ==2:
+        croppedMap = deformMap[psp.d//2-psp.h//2:psp.d//2+psp.h//2,psp.d//2-psp.w//2:psp.d//2+psp.w//2]
+    else:
+        raise UserWarning("Dim of deformMap should be 2 or 3.")
     return croppedMap
+
+def insertMap(deformMap):
+    if np.ndim(deformMap) == 3:
+        channel = deformMap.shape[0]
+        insertedMap = np.zeros((channel,psp.d,psp.d))
+        for c in range(channel):
+            insertedMap[c,psp.d//2-psp.h//2:psp.d//2+psp.h//2,psp.d//2-psp.w//2:psp.d//2+psp.w//2] = deformMap
+    elif np.ndim(deformMap) ==2:
+        insertedMap = np.zeros((psp.d,psp.d))
+        insertedMap[psp.d//2-psp.h//2:psp.d//2+psp.h//2,psp.d//2-psp.w//2:psp.d//2+psp.w//2] = deformMap
+    else:
+        raise UserWarning("Dim of deformMap should be 2 or 3.")
+    return insertedMap
 
 def fill_blank(img):
     # here we assume there are some zero value holes in the image,
@@ -172,13 +190,88 @@ class SuperPosition:
 
         local_kx = psp.d//2
         local_ky = psp.d//2
-
+        # sparse_mask = cropMap(self.sparse_mask)
         all_points = np.where(self.sparse_mask == 1)
         contact_points = np.where((contact_mask == 1) & (self.sparse_mask == 1))
         num_points = contact_points[0].shape[0]
 
         uz = -1*gel_map[(contact_mask == 1) & (self.sparse_mask == 1)]
-        min_z = -1*np.min(uz)
+        # min_z = -1*np.min(uz)
+
+        # correct the motion within contact area
+        activeMapZ = self.correct_KeyZ(contact_points, uz)
+        activeMap = activeMapZ
+        if local_deform[0] != 0.0:
+            ux = np.zeros((num_points))
+            factor = 1
+            if local_deform[0] > 0:
+                factor = -1
+            ux[:] = factor*local_deform[0]/psp.pixmm
+            activeMapX = self.correct_KeyX(contact_points, ux, uz)
+            activeMapX *= -1*factor
+            activeMap += activeMapX
+
+        if local_deform[1] != 0.0:
+            uy = np.zeros((num_points))
+            factor = 1
+            if local_deform[1] > 0:
+                factor = -1
+            uy[:] = factor*local_deform[1]/psp.pixmm
+            activeMapY = self.correct_KeyY(contact_points, uy, uz)
+            activeMapY *= -1*factor
+            activeMap += activeMapY
+
+        # superposition principle to get all node's motion
+        for i in range(all_points[0].shape[0]):
+            qx = all_points[1][i]
+            qy = all_points[0][i]
+            total_deform = np.array([0.0,0.0,0.0])
+            # vectorize
+            kx_list = contact_points[1]
+            ky_list = contact_points[0]
+
+            # get relative position
+            cur_x_list = qx-kx_list+local_kx
+            cur_y_list = qy-ky_list+local_ky
+            mask_x = (cur_x_list >= 0) & (cur_x_list < psp.d)
+            mask_y = (cur_y_list >= 0) & (cur_y_list < psp.d)
+            mask_valid = mask_x & mask_y
+
+            # retrieve the mutual tensors
+            T_list = self.tensorMap[cur_y_list[mask_valid],cur_x_list[mask_valid],:,:]
+            # fraction factors
+            T_list[:,0:2,2] *= pr.normal_friction
+            T_list[:,0:2,0:2] *= pr.shear_friction
+
+            # get the corrected motions for active nodes
+            corrected_deform = activeMap[:,ky_list[mask_valid],kx_list[mask_valid]]
+            cur_deform = np.matmul(T_list, corrected_deform.T[..., np.newaxis])
+            total_deform = np.sum(cur_deform,axis=0)
+            resultMap[:,qy,qx] = total_deform.squeeze()
+        saveMap = cropMap(resultMap)
+        return saveMap
+    
+    def compose_sparse_insert(self, local_deform, _gel_map, _contact_mask):
+        """
+        local deform: (3,1) dx, dy, dz initial displacement
+        gel map: height map
+        contact_mask: indicate contact points
+        return:
+        resultant displacement map (3, d, d) in dx, dy, dz
+        """
+        resultMap = np.zeros((3,psp.d,psp.d))
+
+        local_kx = psp.d//2
+        local_ky = psp.d//2
+        # sparse_mask = cropMap(self.sparse_mask)
+        all_points = np.where(self.sparse_mask == 1)
+        contact_mask = insertMap(_contact_mask)
+        gel_map = insertMap(_gel_map)
+        contact_points = np.where((contact_mask == 1) & (self.sparse_mask == 1))
+        num_points = contact_points[0].shape[0]
+
+        uz = -1*gel_map[(contact_mask == 1) & (self.sparse_mask == 1)]
+        # min_z = -1*np.min(uz)
 
         # correct the motion within contact area
         activeMapZ = self.correct_KeyZ(contact_points, uz)
